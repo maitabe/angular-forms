3a979a888d74725ed6df34b52dfc8b470b496efe
maitabe <martushb@gmail.com>
2016-12-22 10:52:27 +0200

    adding main files

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var port = 8000;
+var app = express();
+app.use(express.static('public'));
+app.use('/node_modules', express.static('node_modules'));
+
+app.listen(port, function() {
+	console.log('server running and healthy');
+});
