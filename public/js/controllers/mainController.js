0ddd4d5f38d840bdce7415efd7406baa986f77f4
maitabe <martushb@gmail.com>
2016-12-22 12:04:03 +0200

    validation attributes

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,3 +1,10 @@
 app.controller('MainCtrl', ['$scope', function($scope) {
 
+	$scope.submit = function(user) {
+		console.log(user);
+
+		// $scope.user.name = '';
+		// $scope.user.email = '';
+	};
+
 }]);
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var port = 8000;
 var app = express();
 app.use(express.static('public'));
-app.use('/node_modules', express.static('node_modules'));
+app.use(express.static('node_modules'));
+
 
 app.listen(port, function() {
 	console.log('server running and healthy');
